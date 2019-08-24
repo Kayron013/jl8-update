bbd0d4d1f39c926a669340db173bcc73a6826d54
Angel <kayron013@gmail.com>
2019-08-24 02:35:25 -0400

    Await final writeDB; Logs & returns

--- index.js
+++ index.js
@@ -7,25 +7,39 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3({});
 const bucket_params = require('./private/bucket.json');
 
+let new_issue = { found: false, issue: null };
+
 exports.handler = async () => {
   let db;
   try {
     db = await getDB();
     const issue = await getCurrentIssue();
-    const is_new_issue = issue > db.issue;
-    if (is_new_issue) {
+    new_issue.found = issue > db.issue;
+    if (new_issue.found) {
       await sendEmail(issue);
       db.issue = issue;
       db.updates++;
     }
     db.last_check = formatDate();
-    db.error = {};
+    db.error = '';
   } catch (e) {
-    console.error('Error:', e);
     if (db) db.error = e;
   } finally {
-    if (db) {
-      writeDB(db);
+    try {
+      if (db) await writeDB(db);
+    } catch (e) {
+      db.error = e;
+    } finally {
+      if (db.error !== '') {
+        console.error('Error:', e);
+        return 'Error: ' + db.error;
+      }
+      if (new_issue.found) {
+        console.log(`New Issue Found {${new_issue.issue}}`);
+        return `New Issue Found {${new_issue.issue}}`;
+      }
+      console.log('No New Issue Found');
+      return 'No New Issue Found';
     }
   }
 };
@@ -38,18 +52,22 @@ const getDB = () =>
     });
   });
 
-const writeDB = db => {
-  s3.putObject(
-    {
-      ...bucket_params,
-      Body: JSON.stringify(db, undefined, 2)
-    },
-    (err, data) => {
-      if (err) console.error(err);
-      if (data) console.log(data);
-    }
-  );
-};
+const writeDB = db =>
+  new Promise((resolve, reject) => {
+    s3.putObject(
+      {
+        ...bucket_params,
+        Body: JSON.stringify(db, undefined, 2)
+      },
+      (err, data) => {
+        if (err) return reject(err);
+        if (data) {
+          console.log('DB Updated');
+          resolve();
+        }
+      }
+    );
+  });
 
 const getCurrentIssue = () =>
   new Promise((resolve, reject) => {
